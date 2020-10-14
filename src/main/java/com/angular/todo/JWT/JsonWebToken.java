efe015d4ace62366c19905e04984584eabeebc21
jerry-tech <53123555+jerry-tech@users.noreply.github.com>
2020-10-14 07:40:35 +0100

    Changing database property to mySql db

--- src/main/java/com/angular/todo/JWT/JsonWebToken.java
+++ src/main/java/com/angular/todo/JWT/JsonWebToken.java
@@ -44,7 +44,7 @@ public class JsonWebToken {
     private String createToken(Map<String, Object> claims, String subject) {
 
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .setExpiration(new Date(System.currentTimeMillis() + 10000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
